a69bcf0ab92c831fa180bff52ef0134535394a33
Layla Rodas <rodas.layla@gmail.com>
2024-02-26 17:04:00 +0100

    restriction publicaLayout

--- src/components/layout/public/PublicLayout.jsx
+++ src/components/layout/public/PublicLayout.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Header } from './Header'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '../../../hooks/useAuth'
 
 export const PublicLayout = () => {
+
+  const {auth} = useAuth();
+
   return (
     <>
         {/* LAYOUT */}
         <Header />
 
         <section className="layout__content">
-          <Outlet />
+          { !auth._id ? <Outlet /> 
+            : <Navigate to="/social" />
+          }
+          
         </section>
     </>
   )
