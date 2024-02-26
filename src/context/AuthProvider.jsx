268a4209961198a81b47bbfa6ed96e937777da99
Layla Rodas <rodas.layla@gmail.com>
2024-02-26 16:25:14 +0100

    sharing user data

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,50 @@
 import React from 'react'
 import { createContext } from 'react'
 import { useState, useEffect } from 'react'
+import { Global } from '../helpers/Global'
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
 
-    const [shared, setShared] = useState("info shared from AuthProvider")
+  const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    authUser();
+  }, []);
+
+  const authUser = async () => {
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('user');
+
+    if(!token || !user){
+      return false
+    }
+
+    const userObj = JSON.parse(user);
+    const userId = userObj.id;
+
+    const request = await fetch(`${Global.url}user/profile/${userId}`,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+
+    const data = await request.json();
+
+    setAuth(data.user);
+
+  };
+
+
+
   return (
-    <AuthContext.Provider value={{shared}}>
+    <AuthContext.Provider value={{
+      auth,
+      setAuth
+    }}>
         {children}
     </AuthContext.Provider>
   )
