1993899e0f49aa75620d1e4165505e1b42f12d8d
Layla Rodas <rodas.layla@gmail.com>
2024-02-27 12:03:22 +0100

    show default value -- settings form

--- src/components/user/Config.jsx
+++ src/components/user/Config.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
+import { useAuth } from '../../hooks/useAuth';
+import {Global} from '../../helpers/Global'
 
 export const Config = () => {
 
     const [saved, setSaved] = useState("not_sent");
+    const { auth } = useAuth();
 
     const updateUser = (e) => {
         e.preventDefault();
+        console.log(auth);
     };
 
     return (
@@ -23,31 +27,36 @@ export const Config = () => {
                 <form action="" className='config__form' onSubmit={updateUser}>
                     <div className='form__group'>
                         <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id=""  />
+                        <input type="text" name="name" id="" defaultValue={auth.name} />
                     </div>
                     <div className='form__group'>
                         <label htmlFor="surname">Last Name</label>
-                        <input type="text" name="surname" id=""  />
+                        <input type="text" name="surname" id="" defaultValue={auth.surname} />
                     </div>
                     <div className='form__group'>
                         <label htmlFor="username">Username</label>
-                        <input type="text" name="username" id=""  />
+                        <input type="text" name="username" id="" defaultValue={auth.username} />
+                    </div>
+                    <div>
+                        <label htmlFor="bio">Bio</label>
+                        <textarea name="bio" id="" defaultValue={auth.bio} />
                     </div>
                     <div className='form__group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id=""  />
+                        <input type="email" name="email" id="" defaultValue={auth.email} />
                     </div>
                     <div className='form__group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id=""  />
+                        <input type="password" name="password" id="" />
                     </div>
                     <div className='form-group'>
                         <label htmlFor="file">Avatar</label>
-                        <div className='avatar'>
-
+                        <div className="general-info__container-avatar">
+                            {auth.image != "default.png" && <img src={Global.url + "user/avatar/"+auth.image} className="container-avatar__img" alt="Profile Picture" />}
+                            {auth.image == "default.png" && <img src={avatar} className="container-avatar__img" alt="Foto de perfil" />}
                         </div>
+                        <br />
                         <input type="file" name='file' id='file' />
-
                     </div>
                     <br />
                     <input type="submit" value="Register" className='btn btn-success' />
