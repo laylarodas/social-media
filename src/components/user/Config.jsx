72631c0c6bcbd725072d0d05b2b526d1c041c4d3
Layla Rodas <rodas.layla@gmail.com>
2024-02-27 12:24:13 +0100

    save data -- user updated //settings

--- src/components/user/Config.jsx
+++ src/components/user/Config.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
 import { useAuth } from '../../hooks/useAuth';
 import {Global} from '../../helpers/Global'
+import { SerializeForm } from '../../helpers/SerializeForm';
 
 export const Config = () => {
 
     const [saved, setSaved] = useState("not_sent");
-    const { auth } = useAuth();
+    const { auth, setAuth } = useAuth();
 
-    const updateUser = (e) => {
+    const updateUser = async (e) => {
         e.preventDefault();
-        console.log(auth);
+        let newDataUser = SerializeForm(e.target);
+
+        delete newDataUser.file;
+
+        const request = await fetch(Global.url + "user/update",{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            },
+            body: JSON.stringify(newDataUser)
+
+        })
+
+        const response = await request.json();
+        if(response.status === "success"){
+            delete response.user.password;
+
+            setAuth(response.user);
+            setSaved("saved");
+            console.log(auth)
+        }else{
+            setSaved("error");
+        }
     };
 
     return (
@@ -20,8 +44,8 @@ export const Config = () => {
 
             <div className="content__posts">
                 {
-                    saved === "saved" ? <strong className='message__success'>User saved</strong> :
-                        saved === "error" ? <strong className='message__error'>User not saved</strong> : ""
+                    saved === "saved" ? <strong className='message__success'>User updated</strong> :
+                        saved === "error" ? <strong className='message__error'>User not updated</strong> : ""
                 }
 
                 <form action="" className='config__form' onSubmit={updateUser}>
@@ -59,7 +83,7 @@ export const Config = () => {
                         <input type="file" name='file' id='file' />
                     </div>
                     <br />
-                    <input type="submit" value="Register" className='btn btn-success' />
+                    <input type="submit" value="Update" className='btn btn-success' />
 
                 </form>
             </div>
