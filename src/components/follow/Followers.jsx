c215d7585a51cff520ed4ad2c23e4ba56061343d
Layla Rodas <rodas.layla@gmail.com>
2024-02-28 12:18:25 +0100

    creating page followers

--- src/components/follow/Followers.jsx
+++ src/components/follow/Followers.jsx
@@ -1,80 +1,94 @@
 import React, { useEffect, useState } from 'react'
 import { Global } from '../../helpers/Global'
 import { UserList } from '../user/UserList'
+import { useParams } from 'react-router-dom';
 
 
 export const Followers = () => {
 
-  const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
-  const [more, setMore] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const [following, setFollowing] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+    const [more, setMore] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [following, setFollowing] = useState([]);
 
-  useEffect(() => {
-    getUsers(1);
-  }, []);
+    const params = useParams();
 
-
-  const getUsers = async (nextPage = 1) => {
-
-    setLoading(true);
-    const request = await fetch(`${Global.url}follow/list/${nextPage}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-      }
-    });
-
-    const response = await request.json();
+    useEffect(() => {
+        getUsers(1);
+    }, []);
 
 
+    const getUsers = async (nextPage = 1) => {
 
-    if (response.status === 'success' && response.users) {
+        setLoading(true);
 
-      let newUsers = response.users;
-
-      if (users.length >= 1) {
-        newUsers = [...users, ...response.users];
-      }
+        const userId = params.userId;
 
 
-      setUsers(newUsers);
-      setLoading(false);
-      setFollowing(response.userFollowing);
+        const request = await fetch(`${Global.url}follow/followers/${userId}/${nextPage}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            }
+        });
 
-      if (users.length >= (response.total - response.users.length)) {
-        setMore(false);
-      }
+        const response = await request.json();
+
+        let cleanUsers = [];
+
+        response.follows.forEach(follow => {
+            cleanUsers = [...cleanUsers, follow.user];
+        })
+        response.users = cleanUsers;
+        console.log(response.users);
+
+
+        if (response.status === 'success' && response.users) {
+
+            let newUsers = response.users;
+
+            if (users.length >= 1) {
+                newUsers = [...users, ...response.users];
+            }
+
+
+            setUsers(newUsers);
+            setLoading(false);
+            setFollowing(response.userFollowing);
+
+            if (users.length >= (response.total - response.users.length)) {
+                setMore(false);
+            }
+
+        }
+
 
     }
 
 
-  }
-
-  
 
 
-  return (
-    <>
-      <header className="content__header">
-        <h1 className="content__title">People</h1>
-        <button className="content__button">Show news</button>
-      </header>
+    return (
+        <>
+            <header className="content__header">
+                <h1 className="content__title">NAME, my followers</h1>
+                <button className="content__button">Show news</button>
+            </header>
 
-      <UserList users={users} 
-                getUsers={getUsers} 
+            <UserList users={users}
+                getUsers={getUsers}
                 following={following}
                 setFollowing={setFollowing}
                 loading={loading}
                 more={more}
                 page={page}
                 setPage={setPage}
-      />
+            />
 
 
-      <br />
-    </>
-  )
+            <br />
+        </>
+    )
 }
