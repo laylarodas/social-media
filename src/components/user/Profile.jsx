5fa62fabc6031f4762b9dfa3bb9c297ff328da2e
Layla Rodas <rodas.layla@gmail.com>
2024-02-28 23:05:29 +0100

    abstraction -- component publications list

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 import { Global } from '../../helpers/Global'
 import { Link } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
+import { PublicationList } from '../publication/PublicationList'
 
 export const Profile = () => {
 
@@ -126,14 +127,14 @@ export const Profile = () => {
             if (!newProfile && publications.length >= (response.total - response.publications.length)) {
                 setMore(false);
             }
+
+            if(response.pages <= 1){
+                setMore(false);
+            }
         }
     }
 
-    const nextPage = () => {
-        let next = page + 1;
-        setPage(next);
-        getPublications(next);
-    }
+   
 
     return (
         <>
@@ -191,67 +192,15 @@ export const Profile = () => {
             </header>
 
 
-
-
-
-            <div className="content__posts">
-
-                {publications.map((publication, index) => {
-
-                    return (
-
-                        <article className="posts__post" key={publication._id}>
-
-                            <div className="post__container">
-
-                                <div className="post__image-user">
-                                    <Link to={"/social/profile/" + publication.user._id} className="post__image-link">
-                                        {publication.user.image != "default.png" && <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Profile Picture" />}
-                                        {publication.user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-
-                                    </Link>
-                                </div>
-
-                                <div className="post__body">
-
-                                    <div className="post__user-info">
-                                        <a href="#" className="user-info__name">{publication.user.name} {publication.user.surname}</a>
-                                        <span className="user-info__divider"> | </span>
-                                        <a href="#" className="user-info__create-date">{publication.created_at}</a>
-                                    </div>
-
-                                    <h4 className="post__content">{publication.text}</h4>
-
-                                </div>
-
-                            </div>
-
-                            {auth._id == publication.user._id &&
-                                <div className="post__buttons">
-
-                                    <a href="#" className="post__button">
-                                        <i className="fa-solid fa-trash-can"></i>
-                                    </a>
-
-                                </div>
-                            }
-
-
-                        </article>)
-                }
-                )}
-
-            </div>
-            <br />
-            {more &&
-                <div className="content__container-btn">
-                    <button className="content__btn-more-post" onClick={nextPage}>
-                        Show more publications
-                    </button>
-                </div>
-            }
-
-            <br />
+            <PublicationList
+                publications={publications}
+                page={page}
+                setPage={setPage}
+                more={more}
+                setMore={setMore}
+                getPublications={getPublications} 
+            />
+            
 
         </>
     )
