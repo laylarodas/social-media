4ef7437ff648f2d01665d8fc59dd729fd8f544e1
Layla Rodas <rodas.layla@gmail.com>
2024-02-28 12:44:35 +0100

    save post

--- src/components/layout/private/Sidebar.jsx
+++ src/components/layout/private/Sidebar.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import avatar from '../../../assets/img/user.png'
 import useAuth from '../../../hooks/useAuth'
 import { Global } from '../../../helpers/Global';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../../hooks/useForm';
+
 
 export const Sidebar = () => {
 
     const { auth, counters } = useAuth();
-    //console.log(auth, counters);
+    const {form, handleInputChange} = useForm({});
+    const [stored, setStored] = useState("not-stored");
+
+
+    const savePost = async (e) => {
+        e.preventDefault();
+        
+        let newPost = form;
+
+        newPost.user = auth._id;
+
+        const request = await fetch(Global.url + "publication/save", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            },
+            body: JSON.stringify(newPost)
+        });
+
+
+        const response = await request.json();
+   
+        if(response.status === "success"){
+            setStored("stored");
+        }else{
+            setStored("error");
+        }
+    }
     return (
         <aside className="layout__aside">
 
@@ -62,19 +92,24 @@ export const Sidebar = () => {
 
                 <div className="aside__container-form">
 
-                    <form className="container-form__form-post">
+                {
+                    stored === "stored" ? <strong className='message__success'>Successfully posted</strong> :
+                    stored === "error" ? <strong className='message__error'>Not posted</strong> : ""
+                }
+
+                    <form className="container-form__form-post" onSubmit={savePost}>
 
                         <div className="form-post__inputs">
-                            <label htmlFor="post" className="form-post__label">What are you thinking today?</label>
-                            <textarea name="post" className="form-post__textarea"></textarea>
+                            <label htmlFor="text" className="form-post__label">What are you thinking today?</label>
+                            <textarea name="text" className="form-post__textarea" onChange={handleInputChange}/>
                         </div>
 
                         <div className="form-post__inputs">
-                            <label htmlFor="image" className="form-post__label">Upload your image</label>
-                            <input type="file" name="image" className="form-post__image" />
+                            <label htmlFor="file" className="form-post__label">Upload your image</label>
+                            <input type="file" name="file" id="file" className="form-post__image" />
                         </div>
 
-                        <input type="submit" value="Send" className="form-post__btn-submit" disabled />
+                        <input type="submit" value="Send" className="form-post__btn-submit" />
 
                     </form>
 
